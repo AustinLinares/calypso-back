cd579b0bc3e6296d83d3cae87297654df494f607
Diego Linares <diegojeanluck@hotmail.com>
2023-12-03 03:48:33 -0500

    Add: Admin bootstrap (first user)

---
+++ src/admin.bootstrap.ts
@@ -0,0 +1,34 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { WorkersService } from './workers/workers.service';
+import { AppModule } from './app.module';
+import { Role } from './role/enums/role.enum';
+
+export async function bootstrapAdmin() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const workersService = app.get(WorkersService);
+  const configService = app.get(ConfigService);
+
+  const adminEmail = configService.get<string>('ADMIN_EMAIL');
+
+  try {
+    await workersService.getByEmail(adminEmail);
+    console.log('Admin user already exists.');
+  } catch (error) {
+    await workersService.create({
+      email: adminEmail,
+      first_name: 'admin',
+      last_name: 'admin',
+      phone: configService.get<string>('ADMIN_PHONE'),
+      role: Role.ADMIN,
+      speciality: '',
+      presentation: 'The first admin',
+      services_ids: [],
+      rooms_schedules_ids: [],
+    });
+    console.log('Admin user created successfully.');
+  }
+
+  await app.close();
+}
