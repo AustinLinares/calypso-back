d34a6ef624329b85716268530f3e3890872fead5
Diego Linares <diegojeanluck@hotmail.com>
2023-12-03 19:00:11 -0500

    Remove: logging

--- src/custom-config/type-orm-config/type-orm-config.service.ts
+++ src/custom-config/type-orm-config/type-orm-config.service.ts
@@ -16,7 +16,6 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       database: this.configService.get('DB_NAME') || 'calypso_db',
       autoLoadEntities: true,
       synchronize: this.configService.get('SYNCHRONIZE') === 'true',
-      logging: true,
     };
   }
 }
